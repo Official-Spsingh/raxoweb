ea122020ed1e7f97ca178424b5e1d07e905261bb
official-spsingh <shubhampratp.singh@gmail.com>
2020-01-04 11:50:47 +0530

    grow your buisness section and css fixes delivering digital experiences section

---
+++ raxoweb/src/components/modules/GrowYourBuisness/index.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const image = require('../../../media/growBuisness.jpg')
+const icon1 = require('../../../media/growthicon.png')
+const icon2 = require('../../../media/manageicon.png')
+const icon3 = require('../../../media/experience.png')
+
+const GrowYourBuisness = (props) => {
+    return (
+        <div className="grow-your-buisness">
+            <div className="left-section">
+                <div className="outer-section">
+                    <div className="middle-section">
+                        <div className="image-container">
+                            <img src={image} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right-section">
+                <div className="right-section-container">
+
+
+                    <div className="growth-list-section">
+                        <div className="header-section">
+                            <div className="header-left-section">
+                                <img src={icon1}></img>
+                            </div>
+                            <div className="header-right-secion">
+                                Growth
+        </div>
+
+                        </div>
+                        <div className="content-section">
+                            A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.
+        </div>
+                    </div>
+
+                    <div className="growth-list-section">
+                        <div className="header-section">
+                            <div className="header-left-section">
+                                <img src={icon2}></img>
+                            </div>
+                            <div className="header-right-secion">
+                                Management
+        </div>
+
+                        </div>
+                        <div className="content-section">
+                            A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.
+        </div>
+                    </div>
+
+                    <div className="growth-list-section">
+                        <div className="header-section">
+                            <div className="header-left-section">
+                                <img src={icon3}></img>
+                            </div>
+                            <div className="header-right-secion">
+                                Experience
+        </div>
+
+                        </div>
+                        <div className="content-section">
+                            A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.
+        </div>
+                    </div>
+
+                </div> </div>
+        </div>
+    )
+}
+export default GrowYourBuisness;
--- raxoweb/src/components/modules/LandingPageContainer/index.jsx
+++ raxoweb/src/components/modules/LandingPageContainer/index.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import HeaderComponent from '../HeaderComponent';
 import TitleBoxComponent from '../TitleBoxComponent';
 import HomePageContentContainer from '../HomePageContentContainer';
+import GrowYourBuisness from '../GrowYourBuisness';
 const LandingPageContainer = (props) => {
     return (
         <div className="landing-page-container">
             <HeaderComponent />
             <TitleBoxComponent />
-            <HomePageContentContainer title="Delivering Digital Experience" subTitle="Manage Handle Grow Your Business With Us">
+            <HomePageContentContainer title="Delivering Digital Experience">
+                <p> A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.</p>
+            </HomePageContentContainer>
+            <HomePageContentContainer title="Manage Handle Grow Your Business With Us">
+                <GrowYourBuisness />
+            </HomePageContentContainer>
+            <HomePageContentContainer title="Manage Handle Grow Your Business With Us">
+                <h1>Hello content</h1>
+                <h1>Hello content</h1>
                 <h1>Hello content</h1>
             </HomePageContentContainer>
+
         </div>
     )
 }
