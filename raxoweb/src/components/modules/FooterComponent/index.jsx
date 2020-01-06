9e733923d179257050162df7468f04cb590040eb
official-spsingh <shubhampratp.singh@gmail.com>
2020-01-06 20:53:57 +0530

    footer and mobile ui changes

--- raxoweb/src/components/modules/FooterComponent/index.jsx
+++ raxoweb/src/components/modules/FooterComponent/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Icon} from 'antd';
 const FooterComponent = (props) => {
     return (
         <div className="footer-component">
             
-            Footer
+            <div className="footer">
+                <div className="left-section">
+                    <span>All Rights Reserved &copy; Raxoweb 2020-21</span>
+                </div>
+                <div className="right-section">
+                    <div>
+                        <div style={{background:"#3b5998"}}><Icon type="facebook" theme="filled" /></div>
+                        <div style={{background:"#E1306C"}}><Icon type="instagram" theme="filled" /></div>
+                        <div style={{background:"#2867B2"}}><Icon type="linkedin" theme="filled" /></div>
+                        <div style={{background:"#D44638"}}><Icon type="mail" theme="filled" /></div>
+                        <div style={{background:"#111111"}}><Icon type="github" theme="filled" /></div>
+                    </div>
+                </div>
+                
+                </div>
 
         </div>
     )
