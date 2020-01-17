4151a6cd44030a3cf56891af5289723720bca1e9
raxoweb <raxoweb@gmail.com>
2020-01-17 13:19:07 +0530

    gmial api is added

--- raxoweb_apis/resources/user.py
+++ raxoweb_apis/resources/user.py
@@ -5,6 +5,9 @@ import random
 import datetime
 import smtplib
 from flask_mail import Mail, Message
+
+current_time = datetime.datetime.now()
+
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
@@ -22,11 +25,11 @@ class UserRegister(Resource):
                         required=True,
                         help="This field cannot be blank."
                         )
-      
+
     def post(self):
         data = UserRegister.parser.parse_args()
-        if UserModel.find_by_username(data['username']):
-            return {"message": "A user with that username already exists"}, 400
+        if UserModel.find_by_username(data['email']):
+            return {"message": "A user with this email already exists"}, 400
         try:
             user = UserModel(data['username'], data['password'], data['email'])
             user.save_to_db()
@@ -38,3 +41,72 @@ class UserRegister(Resource):
         except:
             return {"message": "can't able to create user, please try again"}
         return {"message": "User created successfully."}, 201
+
+class UserLogin(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    def post(self):
+        data = UserLogin.parser.parse_args()
+        email = data['email']
+        password = data['password']
+        valid_user = UserModel.find_by_email(email)
+        if not valid_user:
+            return { "data":{
+                        "message": "You don't have user account, please create one first"
+                            },
+                    "status":{
+                    "code":400,
+                    "value":"user not found"
+                    }
+                }, 200
+        if  valid_user.isactive:
+            if valid_user.password == password:
+                return {
+                "data":{
+                        "username":valid_user.username
+                    },
+                "status":{
+                "code":200,
+                "value":"login successfully"
+                 }
+                }, 200
+            else:
+                return {
+                    "data":{
+                        "username":valid_user.username
+                    },
+                    "status":{
+                        "code":400,
+                        "value":"Wrong password"
+                 }
+                }, 200
+        else:
+            if ValidateUser.find_by_email(email).expiretime < current_time:
+                return {
+                    "data":{
+                        "username":valid_user.username
+                    },
+                    "status":{
+                        "code":405,
+                        "value":"validate pincode"
+                 }
+                }, 200
+            else:
+                return {
+                    "data":{
+                        "username":valid_user.username
+                    },
+                    "status":{
+                        "code":405,
+                        "value":"passcode expire, please signup again."
+                 }
+                }, 200
