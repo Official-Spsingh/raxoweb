7cd027664a32cf4f3b46175863a4d1344cf6c13e
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2020-01-24 11:42:36 +0530

    blogs

--- raxoweb/src/components/modules/BlogsComponent/index.jsx
+++ raxoweb/src/components/modules/BlogsComponent/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { Icon } from "antd";
+import { Button } from "antd";
+import { withRouter } from "react-router";
 const BlogsComponent = props => {
+  const gotoPage = link => {
+    props.history.push(link);
+  };
   return (
     <div className="blogs-component">
-      Blog component Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Fuga minus corporis expedita et ratione eligendi animi architecto fugiat
-      laudantium enim, aspernatur quis itaque consequatur assumenda deserunt
-      culpa corrupti soluta molestias.
+      <p>
+        Blog component Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Fuga minus corporis expedita et ratione eligendi animi architecto fugiat
+        laudantium enim, aspernatur quis itaque consequatur assumenda deserunt
+        culpa corrupti soluta molestias.
+      </p>
+      <div className="btn-container">
+        <Button type="primary" shape="round" onClick={() => gotoPage("blogs")} style={{background:"#0a3d62" ,color:"#fafafa",border:"0px"}}>
+          Read Blogs
+        </Button>
+      </div>
     </div>
   );
 };
-export default BlogsComponent;
+export default withRouter(BlogsComponent);
