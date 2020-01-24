1c0ff6b1e67aa054044e95ba5d929f13826135bd
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2020-01-24 11:18:34 +0530

    forgot password

--- raxoweb/src/components/MainContainer/index.jsx
+++ raxoweb/src/components/MainContainer/index.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import LandingPageContainer from '../modules/LandingPageContainer';
 import HeaderComponent from '../modules/HeaderComponent';
-import { Route, Switch } from "react-router";
+import { Route, Switch, withRouter } from "react-router";
 import BlogsContainer from '../modules/BlogsContainer';
 import HeaderComponentMobile from '../modules/HeaderComponentMobile';
 import LoginSignupContainer from '../modules/LoginSignUpContainer';
 import BootCampContainer from '../modules/BootCampContainer';
 import FooterComponent from '../modules/FooterComponent';
+import { useEffect } from 'react';
 
 const MainContainer = (props) => {
     const [visible, setModalVisible] = useState(false)
      const [activeTab, setActiveTab] = useState("login");
+     useEffect(()=>{
+         console.log(props)
+         if(props.location.pathname=="/blogs"){
+            document.title="Raxoweb Blogs"
+         }
+         else if(props.location.pathname=="/bootcamp"){
+            document.title="Raxoweb Bootcamp"
+         }
+         else{
+             document.title="Raxoweb : Enhance Your Success With Us"
+         }
+
+         
+     })
     return (
         <div className="main-container">
             <HeaderComponent setModalVisible={setModalVisible} setActiveTab={setActiveTab} />
@@ -35,4 +50,5 @@ const MainContainer = (props) => {
         </div>
     )
 }
-export default MainContainer;
+const MainContainerWrapper= withRouter(MainContainer)
+export default MainContainerWrapper;
