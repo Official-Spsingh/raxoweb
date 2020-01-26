955551f483a9e02e146d90815fc66f942c4e250a
Manish <spyde916@gmail.com>
2020-01-26 22:02:16 +0530

    pictures

--- raxoweb/src/components/modules/BootCampContainer/index.jsx
+++ raxoweb/src/components/modules/BootCampContainer/index.jsx
@@ -3,7 +3,7 @@ const BootCampContainer = (props) => {
     return (
         <div className="bootcamp-container">
          <div className="bootcamp-sec1">
-             <img src={require('../.././../media/images/character6.png')}></img>
+             <img src={require('../.././../media/images/character7.png')}></img>
              <div className="bootcamp-sec1-content">
                         <div className="header-sec">
                             Master Your Skills
