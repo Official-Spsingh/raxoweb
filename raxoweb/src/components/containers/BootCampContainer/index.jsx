66b0fad0263d606c7c959999e38da62659d332e3
pawanpatel <patelpawan591@gmail.com>
2020-02-08 12:38:53 +0530

    pawan

--- raxoweb/src/components/containers/BootCampContainer/index.jsx
+++ raxoweb/src/components/containers/BootCampContainer/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import BootCampFirstComponent from '@modules/BootCampFirstComponent';
 import BootCampSecondComponent from '@modules/BootCampSecondComponent';
 import BootCampThirdComponent from '@modules/BootCampThirdComponent';
+import HomePageContentContainer from '@containers/HomePageContentContainer';
+import BootCampFourthComponent from '@modules/BootCampFourthComponent';
 const BootCampContainer = (props) => {
     return (
         <div className="bootcamp-container">
-            <BootCampSecondComponent/>
+            <BootCampSecondComponent />
             <BootCampFirstComponent />
-            <BootCampThirdComponent/>
+            <BootCampThirdComponent />
+            <HomePageContentContainer title="How are we creating the difference">
+                <BootCampFourthComponent />
+            </HomePageContentContainer>
         </div>
     )
 }
---
+++ raxoweb/src/components/modules/BootCampFourthComponent/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BootCampFourthComponent(props) {
+    return (
+        <div>
+            fourth
+        </div>
+    )
+}
+
+export default BootCampFourthComponent
