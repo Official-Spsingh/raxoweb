457da7f5e2d777cc36f46771335e525d6a58d340
pawanpatel <patelpawan591@gmail.com>
2020-02-08 21:27:02 +0530

    login

--- raxoweb/src/components/containers/BootCampContainer/index.jsx
+++ raxoweb/src/components/containers/BootCampContainer/index.jsx
@@ -8,16 +8,10 @@ const BootCampContainer = (props) => {
     return (
         <div className="bootcamp-container">
             <BootCampFirstComponent />
-<<<<<<< HEAD
-            <BootCampFourthComponent />
-            <BootCampSecondComponent />
-            <BootCampThirdComponent />
-=======
              <BootCampFourthComponent/>
              <BootCampSecondComponent/>            
              <BootCampThirdComponent/>
              {/* <BootCampFifthContainer/> */}
->>>>>>> 3fac79e00539ac844bf1032ac9d674a2a09c9f0a
         </div>
     )
 }
