0e2069e4affca3cdfd7926d349a6fed6de7aeaa3
Manish <spyde916@gmail.com>
2020-02-11 23:29:37 +0530

    up

--- raxoweb/src/components/modules/BootCamp/index.jsx
+++ raxoweb/src/components/modules/BootCamp/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import { withRouter } from "react-router";
 const bootcamp = require('@media/images/bootcamp.png')
+const bootcamppage =require('@media/images/c3.png')
 const BootCamp = (props) => {
     const gotoPage = (link) => {
         props.history.push(link)
     }
     return (
         <div className="bootcamp-main-container">
-            <div className="content-container" style={{textAlign:"center"}}>
-                <p>Why need to be experienced to have professionalism ?</p>
-                <p>Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you. Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you. Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you.</p>
-                <div className="btn-container">
-                    <Button type="primary" shape="round" onClick={() => gotoPage('bootcamp')}>Learn More</Button>
-                </div>
-            </div>
+
+            <Row>
+                <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+                    <img style={{height:"100%",width:"100%"}} src={bootcamppage}></img>
+                </Col>
+                <Col xl={12} lg={12} md={12} sm={24} xs={24}>
+                    <div className="content-container" style={{ textAlign: "center" }}>
+                        <p>Why need to be experienced to have professionalism ?</p>
+                        <p>Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you. Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you. Come Join us to make you battle ready. We provide you with perfect atmosphere to grow as a professional coder and someone what makes market demand from you.</p>
+                        <div className="btn-container">
+                            <Button type="primary" shape="round" onClick={() => gotoPage('bootcamp')}>Learn More</Button>
+                        </div>
+                    </div>
+                </Col>
+
+            </Row>
+
         </div>
     )
 }
