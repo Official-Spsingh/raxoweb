4a49ebbdf053125430f0fc1053b1e59462b17cb3
official-spsingh <shubhampratp.singh@gmail.com>
2020-02-12 21:35:33 +0530

    wwriteblogs

--- raxoweb/src/components/modules/WriteBlogComponent/index.jsx
+++ raxoweb/src/components/modules/WriteBlogComponent/index.jsx
@@ -13,38 +13,37 @@ class WriteBlogComponent extends Component {
             editorState: EditorState.createEmpty()
         }
     }
-    uploadImageCallBack=(file)=> {
+    uploadImageCallBack = (file) => {
         return new Promise(
-          (resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'https://api.imgur.com/3/image');
-            xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');
-            const data = new FormData();
-            data.append('image', file);
-            xhr.send(data);
-            xhr.addEventListener('load', () => {
-              const response = JSON.parse(xhr.responseText);
-              resolve(response);
-            });
-            xhr.addEventListener('error', () => {
-              const error = JSON.parse(xhr.responseText);
-              reject(error);
-            });
-          }
+            (resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://api.imgur.com/3/image');
+                xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');
+                const data = new FormData();
+                data.append('image', file);
+                xhr.send(data);
+                xhr.addEventListener('load', () => {
+                    const response = JSON.parse(xhr.responseText);
+                    resolve(response);
+                });
+                xhr.addEventListener('error', () => {
+                    const error = JSON.parse(xhr.responseText);
+                    reject(error);
+                });
+            }
         );
-      }
-    readBlogClicked=()=>{
-        
-          this.props.history.push('blogs')
-       
-      }
-      onEditorStateChange= (editorState) => {
+    }
+    postBlog = () => {
+        console.log(draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())))
+    }
+    readBlogClicked = () => {
+        this.props.history.push('blogs')
+    }
+    onEditorStateChange = (editorState) => {
         this.setState({
-          editorState,
-        },()=>{
-            console.log(draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())))
-        });
-      };
+            editorState,
+        })
+    };
     render() {
         const { editorState } = this.state;
         return (
@@ -59,20 +58,20 @@ class WriteBlogComponent extends Component {
                     <button onClick={() => this.readBlogClicked()}>Read Blogs</button>
                 </div>
                 <div className="write-blog-container">
-                <Editor
-          editorState={editorState}
-          wrapperClassName="demo-wrapper"
-          editorClassName="demo-editor"
-          onEditorStateChange={this.onEditorStateChange}
-          toolbar={{
-            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
-          }}
-        />
-       
+                    <Editor
+                        editorState={editorState}
+                        wrapperClassName="demo-wrapper"
+                        editorClassName="demo-editor"
+                        onEditorStateChange={this.onEditorStateChange}
+                        toolbar={{
+                            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
+                        }}
+                    />
+
                 </div>
                 <div className="post-blog">
-            <Button>Post Your Blog !</Button>
-        </div>
+                    <Button onClick={this.postBlog}>Post Your Blog !</Button>
+                </div>
             </div>
         )
     }
