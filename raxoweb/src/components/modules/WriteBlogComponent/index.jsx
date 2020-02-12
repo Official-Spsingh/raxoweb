bd4dff7b2130b8e9851e95ff4becbe04b986770c
official-spsingh <shubhampratp.singh@gmail.com>
2020-02-12 21:20:25 +0530

    wwriteblogs

--- raxoweb/src/components/modules/WriteBlogComponent/index.jsx
+++ raxoweb/src/components/modules/WriteBlogComponent/index.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { Button } from 'antd';
 class WriteBlogComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            editorState: EditorState.createEmpty()
         }
     }
+    uploadImageCallBack=(file)=> {
+        return new Promise(
+          (resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://api.imgur.com/3/image');
+            xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');
+            const data = new FormData();
+            data.append('image', file);
+            xhr.send(data);
+            xhr.addEventListener('load', () => {
+              const response = JSON.parse(xhr.responseText);
+              resolve(response);
+            });
+            xhr.addEventListener('error', () => {
+              const error = JSON.parse(xhr.responseText);
+              reject(error);
+            });
+          }
+        );
+      }
     readBlogClicked=()=>{
         
           this.props.history.push('blogs')
        
       }
+      onEditorStateChange= (editorState) => {
+        this.setState({
+          editorState,
+        },()=>{
+            console.log(draftToHtml(convertToRaw(this.state.editorState.getCurrentContent())))
+        });
+      };
     render() {
+        const { editorState } = this.state;
         return (
             <div className="blogs-container">
                 <div className="blogs-header">
@@ -24,6 +58,21 @@ class WriteBlogComponent extends Component {
         </div>
                     <button onClick={() => this.readBlogClicked()}>Read Blogs</button>
                 </div>
+                <div className="write-blog-container">
+                <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
+          }}
+        />
+       
+                </div>
+                <div className="post-blog">
+            <Button>Post Your Blog !</Button>
+        </div>
             </div>
         )
     }
